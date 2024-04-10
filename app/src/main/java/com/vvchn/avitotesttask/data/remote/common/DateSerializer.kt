9d48ec061ca02cfedbf0312a0a9ad862a5e72a5b
vvchn <vl.soldatov4@gmail.com>
2024-04-11 01:43:57 +0300

    Suppress type mismatch warning

--- app/src/main/java/com/vvchn/avitotesttask/data/remote/common/DateSerializer.kt
+++ app/src/main/java/com/vvchn/avitotesttask/data/remote/common/DateSerializer.kt
@@ -12,5 +12,5 @@ import java.util.Date
 object DateSerializer : KSerializer<Date> {
     override val descriptor = PrimitiveSerialDescriptor("Date", PrimitiveKind.STRING)
     override fun serialize(encoder: Encoder, value: Date) = encoder.encodeString(value.toString())
-    override fun deserialize(decoder: Decoder): Date = DateFormat.getDateTimeInstance().parse(decoder.decodeString())
+    override fun deserialize(decoder: Decoder): Date = DateFormat.getDateTimeInstance().parse(decoder.decodeString())!!
 }
